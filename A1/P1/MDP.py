863b64366dacf4d23ed34dff260bff8eb9a6edbd
Angelo92Git <52296518+Angelo92Git@users.noreply.github.com>
2022-09-17 19:15:14 -0400

    Add Assignment  1, Part 1

---
+++ A1/P1/MDP.py
@@ -0,0 +1,183 @@
+import numpy as np
+
+class MDP:
+    '''A simple MDP class.  It includes the following members'''
+
+    def __init__(self,T,R,discount):
+        '''Constructor for the MDP class
+
+        Inputs:
+        T -- Transition function: |A| x |S| x |S'| array
+        R -- Reward function: |A| x |S| array
+        discount -- discount factor: scalar in [0,1)
+
+        The constructor verifies that the inputs are valid and sets
+        corresponding variables in a MDP object'''
+
+        assert T.ndim == 3, "Invalid transition function: it should have 3 dimensions"
+        self.nActions = T.shape[0]
+        self.nStates = T.shape[1]
+        assert T.shape == (self.nActions,self.nStates,self.nStates), "Invalid transition function: it has dimensionality " + repr(T.shape) + ", but it should be (nActions,nStates,nStates)"
+        assert (abs(T.sum(2)-1) < 1e-5).all(), "Invalid transition function: some transition probability does not equal 1"
+        self.T = T
+        assert R.ndim == 2, "Invalid reward function: it should have 2 dimensions" 
+        assert R.shape == (self.nActions,self.nStates), "Invalid reward function: it has dimensionality " + repr(R.shape) + ", but it should be (nActions,nStates)"
+        self.R = R
+        assert 0 <= discount < 1, "Invalid discount factor: it should be in [0,1)"
+        self.discount = discount
+        
+    def valueIteration(self,initialV,nIterations=np.inf,tolerance=0.01):
+        '''Value iteration procedure
+        V <-- max_a R^a + gamma T^a V
+
+        Inputs:
+        initialV -- Initial value function: array of |S| entries
+        nIterations -- limit on the # of iterations: scalar (default: infinity)
+        tolerance -- threshold on ||V^n-V^n+1||_inf: scalar (default: 0.01)
+
+        Outputs: 
+        V -- Value function: array of |S| entries
+        iterId -- # of iterations performed: scalar
+        epsilon -- ||V^n-V^n+1||_inf: scalar'''
+
+        V = initialV
+        epsilon = np.inf
+        iterId = 0
+        while epsilon > tolerance and iterId < nIterations:
+            #V1 = np.amax([self.R[0] + self.discount*(self.T[0]@V),self.R[1] + self.discount*(self.T[1]@V)],axis=0)
+            V1 = np.amax(self.R + self.discount*(self.T@V), axis=0)
+            epsilon = np.max(np.abs(V1-V))
+            iterId += 1
+            V=V1
+        return [V,iterId,epsilon]
+
+    def extractPolicy(self,V):
+        '''Procedure to extract a policy from a value function
+        pi <-- argmax_a R^a + gamma T^a V
+
+        Inputs:
+        V -- Value function: array of |S| entries
+
+        Output:
+        policy -- Policy: array of |S| entries'''
+
+        policy = np.argmax(self.R + self.discount*(self.T@V), axis=0)
+        return policy 
+
+    def evaluatePolicy(self,policy):
+        '''Evaluate a policy by solving a system of linear equations
+        V^pi = R^pi + gamma T^pi V^pi
+
+        Input:
+        policy -- Policy: array of |S| entries
+
+        Ouput:
+        V -- Value function: array of |S| entries'''
+        
+        # Solving a system of Linear Equations
+        lhs = np.eye(self.nStates) - self.discount*self.T[policy,range(self.nStates)]
+        rhs = self.R[policy,range(self.nStates)]
+        V = np.linalg.solve(lhs,rhs)
+        # TODO: Code an iteration based solution
+        return V
+        
+    def policyIteration(self,initialPolicy,nIterations=np.inf):
+        '''Policy iteration procedure: alternate between policy
+        evaluation (solve V^pi = R^pi + gamma T^pi V^pi) and policy
+        improvement (pi <-- argmax_a R^a + gamma T^a V^pi).
+
+        Inputs:
+        initialPolicy -- Initial policy: array of |S| entries
+        nIterations -- limit on # of iterations: scalar (default: inf)
+
+        Outputs: 
+        policy -- Policy: array of |S| entries
+        V -- Value function: array of |S| entries
+        iterId -- # of iterations peformed by policy iteration: scalar'''
+
+        policy_old = np.inf*np.ones(np.shape(initialPolicy))
+        policy = initialPolicy
+        iterId = 0
+        while any(policy != policy_old):
+            # Policy Evaluation
+            lhs = np.eye(self.nStates) - self.discount*self.T[policy,range(self.nStates)]
+            rhs = self.R[policy,range(self.nStates)]
+            V = np.linalg.solve(lhs,rhs)
+
+            # Policy Improvement
+            policy_old = policy
+            policy = np.argmax(self.R + self.discount*(self.T@V), axis=0)
+            iterId += 1
+        return [policy,V,iterId]
+            
+    def evaluatePolicyPartially(self,policy,initialV,nIterations=np.inf,tolerance=0.01):
+        '''Partial policy evaluation:
+        Repeat V^pi <-- R^pi + gamma T^pi V^pi
+
+        Inputs:
+        policy -- Policy: array of |S| entries
+        initialV -- Initial value function: array of |S| entries
+        nIterations -- limit on the # of iterations: scalar (default: infinity)
+        tolerance -- threshold on ||V^n-V^n+1||_inf: scalar (default: 0.01)
+
+        Outputs: 
+        V -- Value function: array of |S| entries
+        iterId -- # of iterations performed: scalar
+        epsilon -- ||V^n-V^n+1||_inf: scalar'''
+
+        V = initialV
+        epsilon = np.inf
+        iterId = 0
+        while epsilon > tolerance and iterId < nIterations:
+            #V1 = np.amax([self.R[0] + self.discount*(self.T[0]@V),self.R[1] + self.discount*(self.T[1]@V)],axis=0)
+            V1 = self.R[policy,range(self.nStates)]+ self.discount*(self.T[policy, range(self.nStates)]@V)
+            epsilon = np.max(np.abs(V1-V))
+            iterId += 1
+            V=V1
+        return [V,iterId,epsilon]
+
+    def modifiedPolicyIteration(self,initialPolicy,initialV,nEvalIterations=5,nIterations=np.inf,tolerance=0.01):
+        '''Modified policy iteration procedure: alternate between
+        partial policy evaluation (repeat a few times V^pi <-- R^pi + gamma T^pi V^pi)
+        and policy improvement (pi <-- argmax_a R^a + gamma T^a V^pi)
+
+        Inputs:
+        initialPolicy -- Initial policy: array of |S| entries
+        initialV -- Initial value function: array of |S| entries
+        nEvalIterations -- limit on # of iterations to be performed in each partial policy evaluation: scalar (default: 5)
+        nIterations -- limit on # of iterations to be performed in modified policy iteration: scalar (default: inf)
+        tolerance -- threshold on ||V^n-V^n+1||_inf: scalar (default: 0.01)
+
+        Outputs: 
+        policy -- Policy: array of |S| entries
+        V -- Value function: array of |S| entries
+        iterId -- # of iterations peformed by modified policy iteration: scalar
+        epsilon -- ||V^n-V^n+1||_inf: scalar'''
+
+        policy = initialPolicy
+        V = initialV
+        iterId = 0
+        eval_iterId = 0
+        epsilon = np.inf
+        while epsilon > tolerance and iterId < nIterations:
+            while eval_iterId < nEvalIterations:
+                V1 = self.R[policy,range(self.nStates)]+ self.discount*(self.T[policy, range(self.nStates)]@V)
+                eval_iterId += 1
+                V=V1
+
+            policy = np.argmax(self.R + self.discount*(self.T@V), axis=0)
+            V1 = np.amax(self.R + self.discount*(self.T@V), axis=0)
+            epsilon = np.max(np.abs(V1-V))
+            eval_iterId = 0
+            iterId += 1
+
+
+        # temporary values to ensure that the code compiles until this
+        # function is coded
+        #policy = np.zeros(self.nStates)
+        #V = np.zeros(self.nStates)
+        #iterId = 0
+        #epsilon = 0
+
+        return [policy,V,iterId,epsilon]
+        
---
+++ A1/P1/TestMDP.py
@@ -0,0 +1,31 @@
+from MDP import *
+
+''' Construct simple MDP as described in Lecture 1b Slides 17-18'''
+# Transition function: |A| x |S| x |S'| array
+# First set of arrays is for action "Advertise", the next is for action "Save"
+# Array entry 0: P&U, 1: P&F, 2: R&U, 3: R&F
+T = np.array([[[0.5,0.5,0,0],[0,1,0,0],[0.5,0.5,0,0],[0,1,0,0]],[[1,0,0,0],[0.5,0,0,0.5],[0.5,0,0.5,0],[0,0,0.5,0.5]]])
+# Reward function: |A| x |S| array
+R = np.array([[0,0,10,10],[0,0,10,10]]) # This is different from the formalization in Sut_Bar since the reward is determined by the current state rather than jointly determined with the next state.
+# Discount factor: scalar in [0,1)
+discount = 0.9        
+# MDP object
+mdp = MDP(T,R,discount)
+
+'''Test each procedure'''
+[V,nIterations,epsilon] = mdp.valueIteration(initialV=np.zeros(mdp.nStates))
+policy = mdp.extractPolicy(V) # Extract policy from value function
+print(f"V for value iteration: {V}\nnIterations: {nIterations}\nepsilon: {epsilon:.5f}\npolicy: {policy}\n")
+
+V = mdp.evaluatePolicy(np.array([1,0,1,0])) # Evaluate policy
+np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
+print(f"V for policy evaluation: {V}\n")
+
+[policy,V,iterId] = mdp.policyIteration(np.array([0,0,0,0]))
+print(f"V for policy iteration: {V}\nnIterations: {iterId}\npolicy: {policy}\n")
+
+[V,iterId,epsilon] = mdp.evaluatePolicyPartially(np.array([1,0,1,0]),np.array([0,10,0,13]))
+print(f"V for partial policy evaluation: {V}\nnIterations: {iterId}\ntolerance: {epsilon:.3f}\n")
+
+[policy,V,iterId,tolerance] = mdp.modifiedPolicyIteration(np.array([1,0,1,0]),np.array([0,10,0,13]))
+print(f"V for modified policy iteration: {V}\nnIterations: {iterId}\ntolerance: {tolerance:.3f}\npolicy: {policy}")
